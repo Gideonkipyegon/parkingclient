27c969a2a9b8354bc3b3b2345227d7cb946ee9e3
Gideonkipyegon <gideonkipyegon837@gmail.com>
2024-02-19 13:54:33 +0300

    monday changes

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -18,43 +18,39 @@ function SignUp() {
   })
   const { register, handleSubmit, formState: { errors } } = useForm({ resolver: yupResolver(schema), });
   const onSubmit = (data) => {
-      // console.log('hello world')
       Axios.post(`${apidomain}/auth/register`, data)
           .then((response) => {
               console.log(response);
-              // response.data.message && alert (response.data.message);
               console.log('success', response.data.message);
               navigate("/Login");
           })
           .catch((response) => {
               console.log(response)
-              // console.log(response.data.response);
-              // alert("failed",response.data.response)
 
           });
   }
   return (
     <div className='signup'>
       <form className='signupform' onSubmit = { handleSubmit(onSubmit)}>
-        <label htmlFor="">Firstname</label>
+        <label htmlFor="">Firstname:</label>
         <input type="text" {...register("FirstName")} placeholder='firstname' />
         <p>{errors.FirstName?.message}</p>
-        <label htmlFor="">Lasttname</label>
+        <label htmlFor="">Lasttname:</label>
         <input type="text" {...register("LastName")} placeholder='lastname' />
         <p>{errors.LastName?.message}</p>
-        <label htmlFor="">Email</label>
+        <label htmlFor="">Email:</label>
         <input type="text" {...register("EmailAddress")} placeholder='email' />
         <p>{errors.Email?.message}</p>
-        <label htmlFor="">Phonenumber</label>
+        <label htmlFor="">Phone no:</label>
         <input type="text" {...register("PhoneNumber")} placeholder='phonenumber' />
         <p>{errors.PhoneNumber?.message}</p>
-        <label htmlFor="">Address</label>
+        <label htmlFor="">Address:</label>
         <input type="text" {...register("Address")} placeholder='address' />
         <p>{errors.Address?.message}</p>
-        <label htmlFor="">Password</label>
+        <label htmlFor="">Password:</label>
         <input type="password" {...register("Password")} placeholder='password' />
         <p>{errors.password?.message}</p>
-        <label htmlFor="">Confirmpassword</label>
+        <label htmlFor="">Confirmpassword:</label>
         <input type="password" {...register("ConfirmPassword")} placeholder='confirmpassword' />
         <p>{errors.ConfirmPassword?.message}</p>
         <button type='submit'>Register</button>
